fb1bc069b340ab7338cb8d1612d00560c06b6e26
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-06-02 16:51:55 +0530

    converted the server side rendering to client side rendering

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { manufacturers } from '@/constants';
 
 
-const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
+const SearchManufacturer = ({selected,setSelected}:SearchManufacturerProps) => {
     const [query, setQuery] = useState("");
     const filteredManufacturers = 
         query === "" 
@@ -18,7 +18,7 @@ const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerPro
         ));
   return (
     <div className='search-manufacturer'>
-        <Combobox value={manufacturer} onChange={setManufacturer}>
+        <Combobox value={selected} onChange={setSelected}>
             <div className="relative w-full">
                 <ComboboxButton className="absolute top-[14px]">
                     <Image
